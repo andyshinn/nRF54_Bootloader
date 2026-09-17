de10b3077610c69f19322a9b51c6ec3ef7e96746
Andy Shinn <andys@andyshinn.as>
2026-09-16 21:20:11 -0500

    BLE name for DFU

--- src/boards/xiao_nrf54lm20a/board.h
+++ src/boards/xiao_nrf54lm20a/board.h
@@ -28,6 +28,7 @@
 //--------------------------------------------------------------------+
 // BLE OTA
 //--------------------------------------------------------------------+
+#define DEVICE_NAME           "XIAODFU"
 #define BLEDIS_MANUFACTURER   "Seeed"
 #define BLEDIS_MODEL          "XIAO nRF54LM20A"
 
